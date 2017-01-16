e889a7f1f333b7292a5c12a18b6be7769ee50fcb
Angus McLeod <angus@mcleod.org.au>
2017-01-16 08:04:35 -0500

    update profile widget

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: discourse-profile-widget
-# about: A profile widget that works with discourse-sidebar-widgets
+# about: A profile widget that works with discourse-layouts
 # version: 0.1
 # authors: Angus McLeod
 
@@ -7,6 +7,6 @@ register_asset 'stylesheets/user-widget.scss'
 
 after_initialize do
   SiteSetting.class_eval do
-    @choices[:sidebar_left_widgets].push('profile')
+    @choices[:layouts_sidebar_left_widgets].push('profile')
   end
 end
